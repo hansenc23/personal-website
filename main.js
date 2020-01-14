1420aa1d264cb2b700c64fc246273a0acf7d848c
HC <HC@Hansens-MacBook-Pro.local>
2020-01-14 23:42:29 +0700

    improve mobile navigation

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-$('#navbar a, .btn').on('click', function(event) {
+$('#navbar a, .btn, .link a').on('click', function(event) {
   if (this.hash !== '') {
     event.preventDefault();
 
@@ -12,3 +12,24 @@ $('#navbar a, .btn').on('click', function(event) {
     );
   }
 });
+
+let showMenu = false;
+
+const menuBtn = document.querySelector('.menu-btn');
+const menuMobile = document.querySelector('.menu-content');
+
+menuBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+  if (!showMenu) {
+    menuBtn.classList.add('close');
+    menuMobile.classList.add('show');
+    showMenu = true;
+    console.log(showMenu);
+  } else {
+    menuBtn.classList.remove('close');
+    menuMobile.classList.remove('show');
+    showMenu = false;
+    console.log(showMenu);
+  }
+}
